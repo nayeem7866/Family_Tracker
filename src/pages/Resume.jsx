57382ba8f59ef23b62462595ff8f3d625c05f71f
nayeem7866 <96180759+nayeem7866@users.noreply.github.com>
2024-02-23 16:10:52 +0530

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
-import pdf from "../assets/Nayem_resume.pdf"
+import pdf from "../assets/Nayeeem_resumeee.pdf"
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
